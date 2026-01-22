598485ef298d0f29f2c9007629d3f1f4760f8638
MDYY <98148643+MDYY1@users.noreply.github.com>
2026-01-23 01:17:12 +0800

    Implement strncpy_from_user_nofault for older kernels
    Add a custom implementation of strncpy_from_user_nofault for compatibility with older kernels.

--- kernel/util.h
+++ kernel/util.h
@@ -2,6 +2,21 @@
 #define __KSU_UTIL_H
 
 #include <linux/types.h>
+#include <linux/uaccess.h>
+#include <linux/mm.h>
+#include <linux/pgtable.h>
+
+// 老内核没有 strncpy_from_user_nofault，提供替代实现
+static long strncpy_from_user_nofault(char *dst, const char __user *src, long count)
+{
+    long res;
+    
+    pagefault_disable();
+    res = strncpy_from_user(dst, src, count);
+    pagefault_enable();
+    
+    return res;
+}
 
 #ifndef preempt_enable_no_resched_notrace
 #define preempt_enable_no_resched_notrace()                                    \
